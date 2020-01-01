8dd7f6786c0cfad35e078ebf6a0ae58299a80b55
tobantal <tobantal@gmail.com>
2020-01-01 16:11:20 +0000

    modified save fake data to repository

--- src/main/java/spring/boot/batch/BatchApplication.java
+++ src/main/java/spring/boot/batch/BatchApplication.java
@@ -22,22 +22,23 @@ public class BatchApplication {
 	// generate and save data
     ProductPagingAndSortingRepository repository = ctx.getBean(ProductPagingAndSortingRepository.class);
 
-    long count = 105_000L;
-    List<Product> products = new ArrayList<>((int)count);
-    int gcCounter = 0;
-    String description = "DESCRIPTION";
-    for(long i = 5000L; i < count; i++) {
-		products.add(new Product(i, String.format("Product-%d", i), description, 1234.5));
-        if(++gcCounter%100_000==0) {
-            System.gc();
-            try {
-                Thread.sleep(3_000L);
-            } catch(InterruptedException ie) {
+    for(long j = 0; j < 3_000_000L; j += 1_000_000L) {
+        long count = 105_000L;
+        List<Product> products = new ArrayList<>((int)count);
+        int gcCounter = 0;
+        String description = "DESCRIPTION";
+        for(long i = 5000L; i < count; i++) {
+            products.add(new Product(i, String.format("Product-%d", i), description, 1234.5));
+        }
+        repository.saveAll(products);
 
-            }
+        System.out.println(">>>>> System.gc()");
+        System.gc();
+        try {
+            Thread.sleep(10_000L);
+        } catch(InterruptedException ie) {
         }
     }
-    repository.saveAll(products);
 
     ImportService importService = ctx.getBean(ImportService.class);
     importService.start();
