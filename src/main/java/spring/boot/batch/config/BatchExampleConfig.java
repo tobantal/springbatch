64f284a776ad3424fb7428e7d035e63dae5fe5d2
tobantal <tobantal@gmail.com>
2020-01-05 09:10:09 +0000

    cleaned code

--- src/main/java/spring/boot/batch/config/BatchExampleConfig.java
+++ src/main/java/spring/boot/batch/config/BatchExampleConfig.java
@@ -1,9 +1,5 @@
 package spring.boot.batch.config;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.function.Function;
-
 import javax.sql.DataSource;
 
 import org.springframework.batch.core.Job;
@@ -12,30 +8,20 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemProcessor;
-import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.batch.item.database.JdbcCursorItemReader;
 import org.springframework.batch.item.file.FlatFileItemReader;
-import org.springframework.batch.item.file.FlatFileItemWriter;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
-import org.springframework.batch.item.file.transform.BeanWrapperFieldExtractor;
-import org.springframework.batch.item.file.transform.DelimitedLineAggregator;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
-import org.springframework.batch.item.file.transform.FieldExtractor;
-import org.springframework.batch.item.file.transform.LineAggregator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.jdbc.core.RowMapper;
 
-import spring.boot.batch.mapper.ProductRowMapper;
 import spring.boot.batch.model.Product;
 import spring.boot.batch.reader.JdbcReader;
-import spring.boot.batch.util.StringHeaderWriter;
 import spring.boot.batch.wrapper.JdbcBatchItemWriterWrapper;
 import spring.boot.batch.writer.CsvWriter;
 
@@ -70,13 +56,6 @@ public class BatchExampleConfig {
         }});
         return reader;
     }
-
-    /*
-     * @Bean public ItemReader<Product> reader() { RepositoryItemReader<Product>
-     * reader = new RepositoryItemReader<>();
-     * reader.setRepository(productPagingAndSortingRepository);
-     * reader.setMethodName("findAll"); return reader; }
-     */
 
     @Bean
     public JdbcCursorItemReader<Product> readerDB(DataSource dataSource) {
--- src/main/java/spring/boot/batch/reader/JdbcReader.java
+++ src/main/java/spring/boot/batch/reader/JdbcReader.java
@@ -3,16 +3,12 @@ package spring.boot.batch.reader;
 import javax.sql.DataSource;
 
 import org.springframework.batch.item.database.JdbcCursorItemReader;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
-import lombok.RequiredArgsConstructor;
 import spring.boot.batch.mapper.ProductRowMapper;
 import spring.boot.batch.model.Product;
 
 @Component
-//@RequiredArgsConstructor
 public class JdbcReader extends JdbcCursorItemReader<Product> {
 
     public JdbcReader(DataSource dataSource) {
