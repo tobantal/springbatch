0fb6dcfcb53a11efc2983a923444012f25153b95
tobantal <tobantal@gmail.com>
2020-01-07 04:44:05 +0000

    clened code

--- src/main/java/spring/boot/batch/config/BatchExampleConfig.java
+++ src/main/java/spring/boot/batch/config/BatchExampleConfig.java
@@ -31,101 +31,10 @@ import spring.boot.batch.writer.JdbcWriter;
 public class BatchExampleConfig {
 
     @Autowired
-    JobBuilderFactory jobBuilderFactory;
-
-    @Autowired
-    StepBuilderFactory stepBuilderFactory;
-
-    @Autowired
-    JdbcReader jdbcReader;
-
-    @Autowired
-    JdbcWriter jdbcWriter;
-
-    //@Autowired
-    //@Qualifier("h2DataSource")
-    //DataSource h2DataSource;
-
-    @Autowired
-    DataSource dataSource;
-
-    @Autowired
-    @Qualifier("identityProcessor")
-    ItemProcessor<Product, Product> identityProcessor;
-
-    @Autowired
-    @Qualifier("blankAddressProcessor")
-    ItemProcessor<Product, Product> blankAddressProcessor;
-
-    @Autowired
-    StepFactory stepFactory;
-
-    @Autowired
-    IdentityProcessor processor;
-
-    @Autowired
     JobFactory jobFactory;
 
     @Bean
-    public FlatFileItemReader<Product> readerCsv() {
-        return new CsvReader("import.csv");
-    }
-
-    @Bean
-    public JdbcCursorItemReader<Product> readerDB() {
-        return new JdbcReader(dataSource);
-    }
-
-    @Bean
-    public JdbcBatchItemWriter<Product> writerDB() {
-        return new JdbcWriter(dataSource);
-    }
-
-    @Bean
-    public Step stepCsvToDb() {
-        /*
-        return stepBuilderFactory.get(AppConstants.STEP_CSV_TO_DB)
-                .<Product, Product>chunk(100)
-                .reader(new CsvReader("import.csv")) //
-                .processor(identityProcessor)
-                .writer(jdbcWriter) //WTF ??? new JdbcWriter(dataSource)
-                .build();
-                */
-        /*
-        return stepBuilderFactory.get(AppConstants.STEP_CSV_TO_DB)
-                .<Product, Product>chunk(100)
-                .reader(readerCsv())
-                .processor(identityProcessor)
-                .writer(writerDB())
-                .build();
-*/
-      return stepFactory.createCsvToDbStep("import.csv");
-    }
-
-    @Bean
-    public Step stepDbToCsv() {
-        /*
-        return stepBuilderFactory.get(AppConstants.STEP_DB_TO_CSV)
-                .<Product, Product>chunk(100)
-                .reader(readerDB())
-                .processor(blankAddressProcessor)
-                .writer(new CsvWriter("products-export.csv"))
-                .build();
-                */
-        return stepFactory.createDbToCsvStep("products-export.csv");
-    }
-
-    @Bean
     public Job mainJob() {
-        /*
-        return jobBuilderFactory.get("JOB-" + UUID.randomUUID().toString())
-                .incrementer(new RunIdIncrementer())
-                .flow(stepCsvToDb())
-                .on(AppConstants.STEP_COMPLETED).to(stepDbToCsv())
-                .on(AppConstants.STEP_FAILED).fail()
-                .end()
-                .build();
-                */
         return jobFactory.createComplexJob("COMPLEX_JOB", "import.csv", "products-export.csv");
     }
 
